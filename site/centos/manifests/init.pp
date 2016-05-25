9527e551eaab88e78425106ff36b1834d7338fae
Joe Broquard <joe.broquard@gmail.com>
2016-05-25 11:05:43 -0500

    Update init.pp

--- site/centos/manifests/init.pp
+++ site/centos/manifests/init.pp
@@ -1,5 +1,32 @@
 class centos {
+
   Yumrepo {
+    ensure              => 'present',
+    enabled             => '1',
+    gpgcheck            => '1',
+    gpgkey              => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7',
+    priority            => '99',
+    skip_if_unavailable => '1',
+    before              => [ Package['nginx'], Package['openssl-libs'] ],
+  }
   
+  yumrepo { 'base':
+    descr               => 'CentOS-$releasever - Base',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=os&infra=$infra',
+  }
+  
+  yumrepo { 'updates':
+    descr               => 'CentOS-$releasever - Updates',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=updates&infra=$infra',
+  }
+  
+  yumrepo { 'extras':
+    descr               => 'CentOS-$releasever - Extras',
+    mirrorlist          => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=extras&infra=$infra',
+  }
+  
+  yumrepo { 'centosplus':
+    descr      => 'CentOS-$releasever - Plus',
+    mirrorlist => 'http://mirrorlist.centos.org/?release=$releasever&arch=$basearch&repo=centosplus&infra=$infra',
   }
 }
