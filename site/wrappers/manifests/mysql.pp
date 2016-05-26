8cb882fd0708fa3df880f05067216781cf8c9b79
Joe Broquard <joe.broquard@gmail.com>
2016-05-26 14:41:01 -0500

    Update mysql.pp

--- site/wrappers/manifests/mysql.pp
+++ site/wrappers/manifests/mysql.pp
@@ -1,5 +1,7 @@
 class wrappers::mysql {
-  class { '::mysql' :
-    
+  class { '::mysql::server':
+    root_password           => '!My$QL',
+    remove_default_accounts => true,
+    override_options        => $override_options
   }
 }
