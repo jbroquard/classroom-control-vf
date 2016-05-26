de13a8ff6361f75f7e7a85eb9ef62e662b0c8eb4
Joe Broquard <joe.broquard@gmail.com>
2016-05-26 10:15:03 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,3 +1,29 @@
 class nginx::params {
-
+  case $::osfamily {
+    'redhat','debian': {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
+    }
+    'windows': {
+      $package = 'nginx-service'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
+      $docroot = 'c:/ProgramData/nginx/html'
+      $confdir = 'c:/ProgramData/nginx'
+      $logdir  = 'c:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("Module ${module_name} is not intended to run on ${::osfamily}")
+    }
+  }
+  
+  $runas_user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
+  }
 }
